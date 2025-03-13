2b747ad7b4ee4d64489068e2df8a2354901b828f
Xiao.ZY <2966094090@qq.com>
2025-03-13 19:56:49 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ToBeATopEngineer_AIConversation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
